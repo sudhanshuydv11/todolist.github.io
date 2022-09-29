28000080f80386492a57e9c4effb64be305145d9
sudhanshuydv11 <sudhanshu.sy2000@gmail.com>
2022-09-29 16:52:55 +0530

    all_good_29/9

--- js/add_task.js
+++ js/add_task.js
@@ -3,6 +3,6 @@ let y = document.getElementById("add_button");
 x.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
         y.click();
-        x.value = "";
+
     }
 });
--- js/content_sript.js
+++ js/content_sript.js
@@ -5,13 +5,17 @@
 let counter_for_checkbox = 2;
 
 function Add() {
-    let y = document.getElementById("add_input").value;
+
+    let y1 = document.getElementById("add_input");
+    let y = y1.value;
+    if (y === "") return;
     console.log(y);
     let x = document.getElementById("card_container");
 
 
     let main_child = document.createElement('div');
     let input_div = document.createElement('div');
+    let text_div = document.createElement('div');
     let c_input = document.createElement('input');
     let c_text = document.createElement('text');
 
@@ -19,17 +23,30 @@ function Add() {
     main_child.id = "card" + counter_for_checkbox.toString();
 
     c_input.type = "checkbox";
-
-
+    input_div.className = "common_div";
+    text_div.className = "common_div";
 
     c_input.id = counter_for_checkbox.toString();
     c_text.innerText = y;
+    c_text.className = "custom_text";
     c_text.id = "text" + counter_for_checkbox.toString();
 
     input_div.appendChild(c_input);
+    text_div.appendChild(c_text);
     main_child.appendChild(input_div);
-    main_child.appendChild(c_text);
+    main_child.appendChild(text_div);
     x.appendChild(main_child);
+    setTimeout(() => {
+        c_text.style.opacity = "1";
+        main_child.style.opacity = "1";
+    }, 10);
+
+
+
+
+
+
+
 
     c_input.addEventListener("click", () => {
         let temp = document.getElementById("text" + c_input.id);
@@ -47,7 +64,7 @@ function Add() {
     });
 
 
-
+    y1.value = "";
     counter_for_checkbox++;
 
 }
