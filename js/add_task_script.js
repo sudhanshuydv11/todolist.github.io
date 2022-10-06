e8a4136ccf24f82e120efffb4f0c3642c1bb3efa
sudhanshuydv11 <sudhanshu.sy2000@gmail.com>
2022-10-06 10:14:02 +0530

    webpack_here

--- js/add_task_script.js
+++
@@ -1,8 +0,0 @@
-let x_in = document.getElementById("add_input");
-let y = document.getElementById("add_button");
-x_in.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        y.click();
-
-    }
-});
--- js/other_functions.js
+++
@@ -1,8 +0,0 @@
-function Capital(y) {
-    y = y.trim();
-    s = y[0].toUpperCase();
-    y[0] = "ghgj";
-    return y[0];
-
-}
-console.log(Capital("  dsfsaf    "));
